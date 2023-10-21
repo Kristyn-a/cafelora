ce6a9a502a999790766153d0b1bd294dedd77258
unknown <k.jozifova@gmail.com>
2023-10-21 14:29:44 +0200

    dopoledne

---
+++ src/components/Drink/drink.jsx
@@ -0,0 +1,25 @@
+export const Drink = ({ id, name, ordered, image, layers }) => {
+  return (
+    <div className="drink">
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+          <div className="layer">
+            <div
+              className="layer__color"
+              style={{ backgroundColor: '#613916' }}
+            ></div>
+            <div className="layer__label">espresso</div>
+          </div>
+          <form className="drink__controls">
+            <button className="order-btn">Objednat</button>
+            <input type="hidden" value={id} />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,4 +1,5 @@
 import './menu.css';
+import { Drink } from '../Drink/drink';
 
 export const Menu = () => {
   return (
@@ -8,81 +9,74 @@ export const Menu = () => {
         <p className="menu-intro">
           Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
         </p>
-        <div className="drinks-list">
-          <div className="drink">
-            <div className="drink__product">
-              <div className="drink__cup">
-                <img src="/cups/espresso.png" />
-              </div>
-              <div className="drink__info">
-                <h3>Espresso</h3>
-                <div className="layer">
-                  <div
-                    className="layer__color"
-                    style={{ backgroundColor: '#613916' }}
-                  ></div>
-                  <div className="layer__label">espresso</div>
-                </div>
-              </div>
-            </div>
-            <div className="drink__controls">
-              <button className="order-btn">Objednat</button>
-            </div>
-          </div>
+        <div className="drinks-list"></div>
+        <Drink
+          id={0}
+          name="Espresso"
+          ordered={false}
+          image="https://localhost:4000/assets/cups/espresso.png"
+          layers={[
+            {
+              color: '#613916',
+              label: 'espresso',
+            },
+          ]}
+        />
 
-          <div className="drink">
-            <div className="drink__product">
-              <div className="drink__cup">
-                <img src="/cups/doppio.png" />
-              </div>
-              <div className="drink__info">
-                <h3>Doppio</h3>
-                <div className="layer">
-                  <div
-                    className="layer__color"
-                    style={{ backgroundColor: '#613916' }}
-                  />
-                  <div className="layer__label">espresso</div>
-                </div>
-              </div>
+        {/* <div className="drink">
+          <div className="drink__product">
+            <div className="drink__cup">
+              <img src="/cups/doppio.png" />
             </div>
-            <div className="drink__controls">
-              <button className="order-btn">Objednat</button>
+            <div className="drink__info">
+              <h3>Doppio</h3>
+              <div className="layer">
+                <div
+                  className="layer__color"
+                  style={{ backgroundColor: '#613916' }}
+                />
+                <div className="layer__label">espresso</div>
+              </div>
             </div>
           </div>
-
-          <div className="drink">
-            <div className="drink__product">
-              <div className="drink__cup">
-                <img src="/cups/lungo.png" />
-              </div>
-              <div className="drink__info">
-                <h3>Lungo</h3>
-                <div className="layer">
-                  <div
-                    className="layer__color"
-                    style={{ backgroundColor: '#b0dee1' }}
-                  />
-                  <div className="layer__label">voda</div>
-                </div>
-                <div className="layer">
-                  <div
-                    className="layer__color"
-                    style={{ backgroundColor: '#613916' }}
-                  />
-                  <div className="layer__label">espresso</div>
-                </div>
-              </div>
-            </div>
-            <div className="drink__controls">
-              <button className="order-btn">Objednat</button>
-            </div>
-          </div>
+          <form className="drink__controls">
+            <input type="hidden" className="order-id" value="1" />
+            <button className="order-btn">Objednat</button>
+          </form>
         </div>
 
-        <div className="order-detail">
-          <a href="/objednavka">Detail objednávky</a>
-        </div>
+        <div className="drink">
+          <div className="drink__product">
+            <div className="drink__cup">
+              <img src="/cups/lungo.png" />
+            </div>
+            <div className="drink__info">
+              <h3>Lungo</h3>
+              <div className="layer">
+                <div
+                  className="layer__color"
+                  style={{ backgroundColor: '#b0dee1' }}
+                />
+                <div className="layer__label">voda</div>
+              </div>
+              <div className="layer">
+                <div
+                  className="layer__color"
+                  style={{ backgroundColor: '#613916' }}
+                />
+                <div className="layer__label">espresso</div>
+              </div>
+            </div>
+          </div>
+          <form className="drink__controls">
+            <input type="hidden" className="order-id" value="2" />
+            <button className="order-btn">Objednat</button>
+          </form>
+        </div> */}
+      </div>
+
+      <div className="order-detail">
+        <a href="/objednavka">Detail objednávky</a>
       </div>
     </section>
   );
