f7d17b88a428fb068aa3089061d9617a702f0109
unknown <k.jozifova@gmail.com>
2023-10-21 12:35:59 +0200

    cafeLora zrovozneni navigace

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,29 @@
+import './header.css';
+
+export const Header = () => {
+  return (
+    <header id="domu">
+      <div className="header__content container">
+        <div className="site-logo"></div>
+
+        <div className="navigation">
+          <button className="nav-btn"></button>
+          <nav className="rollout-nav nav-closed">
+            <a href="#home" src="#domu">
+              domů
+            </a>
+            <a href="#menu" src="#menu">
+              menu
+            </a>
+            <a href="#gallery" src="#gallery">
+              galerie
+            </a>
+            <a href="#contact" src="#contact">
+              kontakt
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,167 +1,36 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import { Header } from '../components/Header/header';
+import { Footer } from '../components/Footer/footer';
+import { Banner } from '../components/Banner/banner';
+import { Menu } from '../components/Menu/menu';
+import { Gallery } from '../components/Gallery/galery';
+import { Contact } from '../components/Contact/contact';
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
-    <header>
-      <div className="header__content container">
-        <div className="site-logo"></div>
-
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
-        </div>
-
-      </div>
-    </header>
+    <Header />
     <main>
-      <section className="banner">
-        <div className="container">
-          <div className="banner__content">
-            <h1>Přijď to ochutnat!</h1>
-            <p>Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.</p>
-          </div>
-        </div>
-      </section>
-      <section className="menu">
-        <div className="container">
-          <h2>Naše nabídka</h2>
-          <p className="menu-intro">
-            Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
-          </p>
-          <div className="drinks-list">
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/espresso.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Espresso</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/doppio.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Doppio</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/lungo.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Lungo</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#b0dee1' }} />
-                    <div className="layer__label">voda</div>
-                  </div>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="order-detail">
-            <a href="/objednavka">Detail objednávky</a>
-          </div>
-        </div>
-      </section>
-
-      <section className="gallery">
-        <div className="container">
-          <div className="gallery__content">
-            <h2>Nově otevřeno!</h2>
-            <div className="gallery__cols">
-              <p className="gallery__col">
-                Máte chuť na trochu nostalgie? Rádi vás u nás uvidíme. Atmosféra podniku Vás pohltí a přivede do
-                uvolněné nálady.
-              </p>
-              <p className="gallery__col">
-                V přízemí Cafe Lóra se nachází nově vystavěný bar a restaurant, kde Vám nabízíme koktejly z nově
-                navržené řady "Cocktails &amp; Desires". V suterénu najdete music club se světelnou disco podlahou,
-                barem, VIP loungem, a hrají tam ti nejlepší DJ's!
-              </p>
-              <p className="gallery__col">
-                Jsme podnik pro všechny generace. Součástí kavárny je dětský koutek, který zabaví vaše ratolesti
-                abyste si mohli v klidu vychutnat šálek Vaší oblíbené kávy.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="container">
-          <h2>Kde nás najdete</h2>
-          <div className="contact">
-            <div className="contact__address">
-              Na Mýtě 240 <br />
-              514 03 Byšky nad Jizerou <br />
-              Tel: +420 775 334 427
-            </div>
-            <table className="contact__hours">
-              <tr><td>Pondělí</td><td>ZAVŘENO</td></tr>
-              <tr><td>Úterý</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Středa</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Čtvrtek</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Sobota</td><td>9:00 - 02:00</td></tr>
-              <tr><td>Neděle</td><td>9:00 - 02:00</td></tr>
-            </table>
-            <img
-              className="contact__map"
-              src="./img/map.png"
-            />
-          </div>
-        </div>
-      </section>
+      <Banner />
+      <Menu />
+      <Gallery />
+      <Contact />
     </main>
-
-    <footer>
-      <div className="container">
-        <div className="footer__content">
-          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-        </div>
-      </div>
-    </footer>
-  </div>
+    <Footer />,
+  </div>,
 );
+
+//Zobrazování a skrývání navigačního menu uděláme tak, že budeme prvku .rollout-nav přidávat nebo odebírat třídu nav-closed. V hlavním index.jsx vyberte ikonku .nav-btn a připojte k ní posluchač události click. Tento posluchač bude přepínat třídu nav-closed na prvku .rollout-nav. Klikáním na ikonku tak můžeme zobrazovat nebo skrývat navigaci.
+const prepinaniMenu = () => {
+  document.querySelector('.rollout-nav').classList.toggle('nav-closed');
+};
+document.querySelector('.nav-btn').addEventListener('click', prepinaniMenu);
+
+//Navigaci budeme chtít schovat i po kliknutí na odkaz na nějakou sekci. Připojte tedy další posluchač události přímo na prvek .rollout-nav. V posluchači události zařiďte, aby se navigace při kliknutí na libovolnou její položku schovala (tj. prvku .rollout-nav přidáte třídu nav-closed, obdobně, jako v předchozím kroku při přepínání).
+
+const zmizeniMenu = () => {
+  document.querySelector('.rollout-nav').classList.add('nav-closed');
+};
+
+document.querySelector('.rollout-nav').addEventListener('click', zmizeniMenu);
